0322c608ad32f5fb3e919f30786e1329daac6ad3
Unknown <VeronicaLindkvist@gmail.com>
2018-05-28 17:45:05 +0200

    ny database.sql
    tagit bort och lagt till en ny database.sql fil, den ska ligga i hot fix och sedan mergas till master, men eftersom det varbort taget från stora datorn så hittar jag inte det gamla programmet och vet inte hur man gör i det här.

--- Database.sql
+++ Database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `VeronicaL` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci */;
+USE `VeronicaL`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: VeronicaL
@@ -284,6 +286,38 @@ SET character_set_client = utf8;
 SET character_set_client = @saved_cs_client;
 
 --
+-- Dumping events for database 'VeronicaL'
+--
+
+--
+-- Dumping routines for database 'VeronicaL'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `movie_gets_rented` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `movie_gets_rented`(
+p_moviesId int, 
+p_customerId int,
+p_employeeId int
+)
+BEGIN
+INSERT INTO Rented (rentedDate, dueDate, movieReturned, customers_idcustomers,movies_idMovies, employee_idemployee)
+VALUES (curdate(), DATE_ADD(CURDATE(), INTERVAL + 4 DAY), 0, p_customerId, p_moviesId, p_employeeId);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
 -- Final view structure for view `v_employee_rented`
 --
 
@@ -382,4 +416,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-08 14:56:04
+-- Dump completed on 2018-05-28 17:29:08
